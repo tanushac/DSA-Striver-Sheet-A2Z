bd5ebc62935ab04135f6ce6d082a50edab98f912
Tanusha Choudhary <144540796+tanushac@users.noreply.github.com>
2024-04-13 18:54:10 +0530

    Add files via upload

---
+++ section 6.3/middleOfLL.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+/* Definition for singly-linked list.*/
+ struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+ //tortoise hare method(one slow pointer and one fast pointer)
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast != NULL&&fast->next!=NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+       return slow;
+    }
+};
