06612de027cae861759f35894bf7346f68c8f42a
Tanusha Choudhary <144540796+tanushac@users.noreply.github.com>
2024-04-23 22:31:09 +0530

    Create sort012.cpp

---
+++ section 6.3/sort012.cpp
@@ -0,0 +1,61 @@
+/*
+Following is the class structure of the Node class:
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node()
+    {
+        this->data = 0;
+        next = NULL;
+    }
+    Node(int data)
+    {
+        this->data = data; 
+        this->next = NULL;
+    }
+    Node(int data, Node* next)
+    {
+        this->data = data;
+        this->next = next;
+    }
+};
+*/
+
+Node* sortList(Node *head){
+    if(head==NULL||head->next==NULL){
+        return head;
+    }
+Node* zeroHead=new Node(-1);
+Node* zero=zeroHead;
+Node* oneHead=new Node(-1);
+Node* one=oneHead;
+Node* twoHead=new Node(-1);
+Node* two=twoHead;
+Node* temp=head;
+while(temp!=NULL){
+    if(temp->data==0){
+        zero->next=temp;
+        zero=temp;
+    }
+    else if(temp->data==1){
+        one->next=temp;
+        one=temp;
+    }
+    else{
+        two->next=temp;
+        two=temp;
+    }
+    temp=temp->next;
+}
+zero->next=oneHead->next?oneHead->next:twoHead->next;
+one->next=twoHead->next;
+two->next=NULL;
+// Node* newHead=zeroHead->next;
+// delete zeroHead;
+// delete twoHead;
+// delete oneHead;
+return zeroHead->next;
+}
